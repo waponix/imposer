21629dc882c05b5be2e84396e1c678c14b07803b
waponix <eric.bermejo.reyes@gmail>
2023-03-30 23:54:40 +0800

    Added a new Common Rule length

---
+++ src/Rule/Common/Length.php
@@ -0,0 +1,17 @@
+<?php
+namespace Waponix\Imposer\Rule\Common;
+
+use Waponix\Imposer\Rule\Rule;
+use Waponix\Imposer\Attribute\Directive;
+
+#[Directive(
+    id: 'length', 
+    message: 'value\'s length should be exactly %d'
+)]
+class exactStringLength extends Rule
+{
+    public function assert(mixed $data, ?array $args = null): ?bool
+    {
+        return is_string($data) && strlen($data) === $args[0];
+    }
+}
