9c6c526cf60af43cec58313f3f6be90d500f3cc7
waponix <eric.bermejo.reyes@gmail.com>
2023-04-02 00:19:46 +0800

    Fixed bug with the rule parser

--- src/ArrayValidator.php
+++ src/ArrayValidator.php
@@ -20,16 +20,26 @@ class ArrayValidator extends Validator
         $rules = [];
         foreach ($this->rules as $target => $raw) {
             $rules = [...$rules, ...$this->parseStringRule($raw)];
+            $data = $this->getTargetData($target);
 
             foreach ($rules as $rule) {
                 if (!isset($this->directives[$rule['id']])) {
                     throw new NoRuleDefinitionException('The rule ' . $rule['id'] . ' does not exist');
                 }
 
+                if ($rule['id'] !== self::REQUIRED && $data instanceof _ValueNotFound) {
+                    continue; // ignore this, since it is not required
+                }
+
                 $directive = $this->directives[$rule['id']];
                 $assert = $directive->assert;
 
-                if ($assert($this->getTargetData($target), $rule['parameters']) === false) {
+                if ($rule['id'] === self::REQUIRED && $data instanceof _ValueNotFound) {
+                    $this->addError($target, $this->translateById($directive->message, $rule['parameters']));
+                    continue;
+                }
+
+                if ($assert($data, $rule['parameters']) === false) {
                     $this->addError($target, $this->translateById($directive->message, $rule['parameters']));
                 }
             }
