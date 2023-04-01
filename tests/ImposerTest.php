9b9b461b2768cb0b397e7f95dd1b01b06248ee24
waponix <eric.bermejo.reyes@gmail.com>
2023-04-01 12:34:37 +0800

    Added unit testing

---
+++ tests/ImposerTest.php
@@ -0,0 +1,48 @@
+<?php
+include_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use Waponix\Pocket\Pocket;
+use Waponix\Imposer\Imposer;
+
+class ImposerTest extends TestCase
+{
+    private array $data = [
+        'user' => [
+            'name' => 'Bob',
+            'email' => 'bob@email.com',
+            'age' => 20
+        ]
+    ];
+    private function loadImposer(): Imposer
+    {
+        $pocket = new Pocket(root: __DIR__ . '/../src');
+        $imposer = $pocket->get(Imposer::class);
+
+        return $imposer;
+    }
+
+    public function testShouldBeInstanceOfImposer()
+    {
+        $imposer = $this->loadImposer();
+
+        $this->assertInstanceOf(Imposer::class, $imposer);
+    }
+
+    public function testShouldBeAbleToTranslateMessage()
+    {
+        $imposer = $this->loadImposer();
+
+        $validator = $imposer->createFromArray($this->data);
+        $validator
+            ->impose([
+                'user.name' => 'string|notEmpty|length(2)'
+            ])
+            ->validate();
+
+        $error = $validator->getErrors();
+
+        $this->assertIsArray($error);
+        $this->assertSame($error['user']['name'][0], 'value should not be longer than 2');
+    }
+}
